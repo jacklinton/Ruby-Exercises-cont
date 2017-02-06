659f22b8bcddcf84b14e212452697e489a89b075
Jack Linton <jacklinton@mac.com>
2017-02-05 20:08:31 -0500

    Fixed the large majority of the adventure game program except for one point where I need to access the values of specific keys within an array of hashes. Will continue to work on it.

--- 2.3.17/exercizes2.3.17.rb
+++ 2.3.17/exercizes2.3.17.rb
@@ -59,28 +59,23 @@ class Choice
 	attr_accessor :choose
 	attr_accessor :question
 
-	def initialize
+	def initialize(question)
 		@choose = choose
 		@question = question
 	end
 
 	def the_choice
 		p question + " y/n"
-		choose = gets.chomp
-		return choose
+		@choose = gets.chomp
 	end
 end
 
 class Adventure
-	attr_accessor :name
-
-	def initialize
-		@name = name
-	end
+	attr_accessor :pname
 
 	def get_name
 		puts "What is your name?"
-		name = gets.chomp
+		@pname = gets.chomp
 	end
 end
 
@@ -108,66 +103,68 @@ class Story
 
 	def initialize
 		@player = player
-		@party = party
+		@party = Array.new(3){Hash.new}
+		@player = Adventure.new
+		@player.get_name
+		@dylan = Character.new("Dylan Richards", true, false, false, true)
+		@ogi = Character.new("Ogi", true, true, false, true)
+		@ai = Character.new("Allen Iverson", false, false, true, true)
+		@first_choice = Choice.new("Will you take " + @dylan.character_name + " with you on your journey?")
+		@second_choice = Choice.new("Will you allow " + @ogi.character_name + " to join your walking party?")
+		@third_choice = Choice.new("Can AI walk with you?")
+		@fourth_choice = Choice.new("Would you like to try again?")
 	end
 
-	def starts
-		player = Adventure.new
-		player.get_name
-		dylan = Character.new("Dylan Richards", true, false, false, true)
-		ogi = Character.new("Ogi", true, true, false, true)
-		ai = Character.new("Allen Iverson", false, false, true, true)
-		first_choice = Choice.new
-		first_choice.question = "Will you take " + dylan.character_name + "with you on your journey?"
-		second_choice = Choice.new
-		second_choice.question = "Will you allow " + ogi.character_name + " to join your walking party?"
-		third_choice = Choice.new
-		third_choice.question = "Can AI walk with you?"
-		fourth_choice = Choice.new
-		fourth_choice.question = "Would you like to try again?"
-	end
+	
+		
 	def begin_story
-		party = []
-		puts "You, " + " are walking alone in a dark wood."
-		puts "You come across " + dylan.character_name + ", and he asks if he can walk with you."
-		first_choice
-		if first_choice.choose == "y"
-			dylan << party
+		puts "You, " + @player.pname + ", are walking alone in a dark wood."
+		gets.chomp
+		puts "You come across " + @dylan.character_name + ", and he asks if he can walk with you."
+		@first_choice.the_choice
+		if @first_choice.choose == "y"
+			@party[0]=@dylan
 		else 
-			dylan.alive = false
+			@dylan.alive = false
 		end
 
 		second_leg
 	end
 	def second_leg
-		puts "While walking in the woods, you come across a computer that says something on the screen..."
+		puts "You continue walking in the woods, you come across a computer that says something on the screen..."
+		gets.chomp
 		puts "It says 'you must fix this computer or someone will die!"
-		if party.include("dylan")
-			puts dylan.character_name + "quickly fixes the computer."
-			puts ogi.character_name + "comes running to you from out of nowhere. He thanks you for saving him and asks if he can join you as you continue through the woods."
-			second_choice
-			if second_choice.choose == "y"
-				puts ogi.charater_name + " says 'Thankyou so much. I can help you because I am super strong.'"
-				party << ogi
+		gets.chomp
+		if @party.include?(@dylan)
+			gets.chomp
+			puts @dylan.character_name + " quickly fixes the computer!"
+			gets.chomp
+			puts @ogi.character_name + " comes running to you from out of nowhere. He thanks you for saving him and asks if he can join you as you continue through the woods."
+			@second_choice.the_choice
+			if @second_choice.choose == "y"
+				puts @ogi.character_name + " says 'Thankyou so much. I can help you because I am super strong.'"
+				@party[1] = @ogi
 				third_leg
 			else
 				puts "Ogi runs away looking very afraid."
-				ogi.alive = false
+				@ogi.alive = false
 				third_leg
 			end
 		else
 			puts "You don't know how to fix the computer, so you keep walking. You hear someone screaming off in the distance..."
-			ogi.alive = false
+			@ogi.alive = false
 			third_leg
 		end
 	end
 	def third_leg
+		gets.chomp
 		puts "You continue walking, when yo hear footsteps coming toward you..."
-		puts "Suddenly, " + ai.character_name + " emerges from some bushes and asks if he can walk with you through the woods."
-		third_choice
-		if third_choice.choose == "y"
-			puts ai.character_name + " says, 'excellent, I'm carrying a knife so I can stab things.'"
-			party << ai
+		gets.chomp
+		puts "Suddenly, " + @ai.character_name + " emerges from some bushes and asks if he can walk with you through the woods."
+		@third_choice.the_choice
+		if @third_choice.choose == "y"
+			puts @ai.character_name + " says, 'excellent, I'm carrying a knife so I can stab things.'"
+			@party[2] = @ai
 			fourth_leg
 		else
 			puts "AI flies away into the distance on a magical rainbow and you find yourself feeling a bit less safe."
@@ -175,13 +172,21 @@ class Story
 		end
 	end
 	def fourth_leg
+		gets.chomp
 		puts "Suddenly, a tree falls on top of you!"
-		if party.include(dylan)
-			puts dylan.character_name + " says, 'I can't lift that tree!'"
-		elsif party.include(ai)
-			puts ai.character_name + "says, 'I ain't helping you.'"
-		elsif party.include(ogi)
-			puts ogi.character_name + " says, 'I can lift the tree! and he lifts the tree, saving you all... for now."	
+		if @party.include?(@dylan)
+			gets.chomp
+			puts @dylan.character_name + " says, 'I can't lift that tree!'"
+			gets.chomp
+		end
+		if @party.include?(@ai)
+			gets.chomp
+			puts @ai.character_name + "says, 'I ain't helping you.'"
+			gets.chomp
+		end
+		if @party.include?(@ogi)
+			gets.chomp
+			puts @ogi.character_name + " says, 'I can lift the tree! and he lifts the tree, saving you all... for now."	
 			fifth_leg
 		else
 			puts "Nobody can lift the tree so you and the rest of your party die of starvation waiting for help."
@@ -189,28 +194,41 @@ class Story
 		end
 	end
 	def fifth_leg
+		gets.chomp
 		puts "Lucky for you that you saved Ogi and allowed him to join your party!"
+		gets.chomp
 		puts "All of a sudden, a wolf jumps out and starts snarling at you!"
-		if party.length < 2
+		gets.chomp
+		if @party.length < 2
 			puts "You don't have enough people to subdue the wolf and it kills you and anyone else with you!"
 			party_dies
-		elsif party.include(ai)
+		elsif @party.include?(@ai)
 			puts "Some of your friends grab the wolf and hold it down."
-			puts ai.character_name " says, 'I got this!' and he stabs the immobilized wolf to death!"
+			gets.chomp
+			puts @ai.character_name + " says, 'I got this!' and he stabs the immobilized wolf to death!"
+			gets.chomp
 			puts "You have survived the walk through the woods and you all go your separate ways."
-			fourth_choice
+			gets.chomp
+			@fourth_choice.the_choice
+			if @fourth_choice.choose = "y"
+				story = Story.new
+				story.begin_story
+			else
+				puts "Goodbye"
+			end
 		end
-		
 	end
 	def party_dies
+		gets.chomp
 		puts "Everyone in your party has died, including you!"
-		puts player.name + ": Deceased"
-		party.each do |person|
-			puts person + ": Deceased"
+		puts @player.pname + ": Deceased"
+		p @party
+		@party.each do |person|
+			puts person.find { |i| i["character_name"] } + ": Deceased"
 		end
 		puts "The story is over."
-		fourth_choice
-		if fourth_choice.choose = "y"
+		@fourth_choice.the_choice
+		if @fourth_choice.choose = "y"
 			story = Story.new
 			story.begin_story
 		else
@@ -220,5 +238,4 @@ class Story
 end
 
 story = Story.new
-story.starts
 story.begin_story
